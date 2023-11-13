de9a794f11ecdcbf0fe10a6d92f2b49b36e90dfa
premkgupta <premg9554@gmail.com>
2023-11-13 14:00:01 +0530

    added user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,3 @@
-// const asyncHandeler = (fn) => async(req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (err) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
-
 const asyncHandeler = (requestHandler) => {
     (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
