f7c7510eea14821a5ddf099d31aeca1a415cbc24
prem k gupta <premg9554@gmail.com>
2023-12-02 16:19:05 +0530

    fix some bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,7 +16,8 @@ cloudinary.config({
               resource_type: "auto"
           })
           // file has been uploaded successfull
-          console.log("file is uploaded on cloudinary ", response.url);
+        //   console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
           return response;
   
       } catch (error) {
