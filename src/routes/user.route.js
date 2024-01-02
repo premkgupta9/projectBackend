fd651d8e955436728b80f56e8fc11bca818a3030
prem k gupta <premg9554@gmail.com>
2024-01-02 18:11:25 +0530

    add user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changePassword, getChannelProfile, getUser, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccontDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,5 +25,13 @@ router.route("/register").post(
     // secured routes
     router.route("/logout").post(verifyJWT, logoutUser)
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT, changePassword)
+    router.route("/getUser").get(verifyJWT, getUser)
+    router.route("/update-account").patch(verifyJWT, updateAccontDetails)
+    router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+    router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"),updateUserCoverImage)
+
+    router.route("/c/:username").get(verifyJWT, getChannelProfile)
+    router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
